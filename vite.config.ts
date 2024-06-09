6cfd1e05ca1262d258b8780f976b4c890a5702e1
Kirill <kirshuvl@gmail.com>
2024-06-09 18:56:56 +0400

    asdasd

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-// import devtools from 'solid-devtools/vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+
+
+
+
 export default defineConfig({
     plugins: [
         solidPlugin(),
         viteStaticCopy({
             targets: [
-                // Копируем изображения из папки public/icons в dist/icons
                 { src: 'public', dest: '' },
             ],
         }),
